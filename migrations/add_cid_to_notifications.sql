5b84dbd9c6065ceaa5d28a1866b9b7a3f42c5557
Doll <dollspacegay@gmail.com>
2025-10-11 15:20:09 -0500

    removed extraneous files and folders

--- migrations/add_cid_to_notifications.sql
+++
@@ -1,5 +0,0 @@
--- Add cid column to notifications table
-ALTER TABLE notifications ADD COLUMN cid VARCHAR(255);
-
--- Add index on cid for better query performance
-CREATE INDEX idx_notifications_cid ON notifications(cid);
